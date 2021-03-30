02f8cd2fdc16d4e1448af56e0d78eb983c793376
李宁 <365388127@qq.com>
2021-03-30 18:04:44 +0800

    Update home.js

--- app/service/home.js
+++ app/service/home.js
@@ -1,1 +1,20 @@
-1
+import { Service } from 'egg';
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
+export default class CommonService extends Service {
+
+    async getUserList(param) {
+        const { app } = this
+        let { name } = param
+        let data = app.model.UserList.findAll({
+            where: {
+                userName: {
+                    [Op.substring]: name
+                },
+            }
+        });
+        return data;
+    }
+}
+
+module.exports = CommonService
