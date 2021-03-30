8b81e986925c220266fa833301d954020faddfa7
李宁 <365388127@qq.com>
2021-03-30 17:06:05 +0800

    Update home.js

--- app/controller/home.js
+++ app/controller/home.js
@@ -7,6 +7,17 @@ class HomeController extends Controller {
     const { ctx } = this;
     ctx.body = 'hi, egg';
   }
+      async userInfo() {
+        const { ctx } = this;
+        const params = ctx.request.body;
+        try {
+            const data = await this.ctx.service.common.userInfo(params)
+            ctx.$success(data)
+        } catch (error) {
+            ctx.logger.error(error);
+            ctx.$error(error);
+        }
+    }
 }
 
 module.exports = HomeController;
