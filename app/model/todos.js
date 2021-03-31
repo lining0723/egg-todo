bcd6b17f85fa38f6ad3b24e45596f4a477bb471f
李宁 <365388127@qq.com>
2021-03-31 13:50:18 +0800

    Update todos.js

--- app/model/todos.js
+++ app/model/todos.js
@@ -1,46 +1,46 @@
 module.exports = app => {
-    const { STRING,INTEGER,DATE} = app.Sequelize;
-    const Todos = app.model.define('todos',
+  const { STRING, INTEGER, DATE } = app.Sequelize;
+  const Todos = app.model.define('todos',
     {
-        id: {
-            type: INTEGER,
-            allowNull: false,
-            primaryKey: true
-        },
-        openid:{
-            allowNull: false,
-            type: STRING(50)
-        },
-        content:{
-            type: STRING(255)
-        },
-        createTime:{
-            type:DATE,
-            field:"createTime"
-        },
-        remarks:{
-            type: STRING(255)
-        },
-        status:{
-            type: STRING(1)
-        },
-        lever:{
-            type: STRING(1)
-        },
-        endTime:{
-            type:DATE,
-            field:"endTime"
-        },
+      id: {
+        type: INTEGER,
+        allowNull: false,
+        primaryKey: true
+      },
+      openid: {
+        allowNull: false,
+        type: STRING(50)
+      },
+      content: {
+        type: STRING(255)
+      },
+      createTime: {
+        type: DATE,
+        field: "createTime"
+      },
+      remarks: {
+        type: STRING(255)
+      },
+      status: {
+        type: INTEGER(1)
+      },
+      lever: {
+        type: INTEGER(1)
+      },
+      endTime: {
+        type: DATE,
+        field: "endTime"
+      },
     },
     {
-        freezeTableName: true, // Model 对应的表名将与model名相同
-        timestamps: false,
+      freezeTableName: true, // Model 对应的表名将与model名相同
+      timestamps: false,
     }
-    );
-     
-    Todos.associate = function() {
+  );
 
-    };
-     
-    return Todos;
+  Todos.associate = function () {
+
+  };
+
+  return Todos;
 };
