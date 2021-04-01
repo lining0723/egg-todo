e392af79da091dd2e74a300c34f2cbf3afa71020
李宁 <365388127@qq.com>
2021-04-01 09:11:47 +0800

    Update router.js

--- app/router.js
+++ app/router.js
@@ -3,7 +3,11 @@
  */
 module.exports = app => {
   const { router, controller, middleware } = app;
-  //const common = app.middleware.common();
+  //const common = middleware.common();
   router.get('/', controller.home.index);
   router.post('/todoList', controller.home.todoList);
+  router.post('/getOpenid', controller.home.getOpenid);
+  router.post('/create', controller.home.create);
+  router.post('/update', controller.home.update);
+  router.post('/remove', controller.home.remove);
 };
