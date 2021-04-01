5e11d609e4621e13dd61732db43e35ce01f7c6a1
李宁 <365388127@qq.com>
2021-04-01 11:16:47 +0800

    Update config.default.js

--- config/config.default.js
+++ config/config.default.js
@@ -5,13 +5,21 @@
 /**
  * @param {Egg.EggAppInfo} appInfo app info
  */
+
+const path = require('path');
 module.exports = appInfo => {
   /**
    * built-in config
    * @type {Egg.EggAppConfig}
    **/
   const config = exports = {};
-
+  config.cluster = {
+    https: {
+      key: path.join(appInfo.baseDir, 'httpskey/server.key'),
+      cert: path.join(appInfo.baseDir, 'httpskey/server.crt'),
+    },
+  };
+  
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1616826949701_2742';
 
