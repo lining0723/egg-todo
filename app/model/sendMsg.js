a82118e5f1b5bade8e678450804aa9fa98d35699
李宁 <365388127@qq.com>
2021-04-13 19:25:04 +0800

    Update sendMsg.js

--- app/model/sendMsg.js
+++ app/model/sendMsg.js
@@ -1,28 +1,32 @@
 module.exports = app => {
     const { STRING, INTEGER, DATE } = app.Sequelize;
     const SendMsg = app.model.define('send_msg',
-      {
-        id: {
-          type: INTEGER,
-          allowNull: false,
-          primaryKey: true
+        {
+            id: {
+                type: INTEGER,
+                allowNull: false,
+                primaryKey: true
+            },
+            todoId: {
+                allowNull: false,
+                type: INTEGER,
+                field: "todoId"
+            },
         },
-        todoId: {
-          allowNull: false,
-          type: INTEGER
-        },
-      },
-      {
-        freezeTableName: true, // Model 对应的表名将与model名相同
-        timestamps: false,
-      }
+        {
+            freezeTableName: true, // Model 对应的表名将与model名相同
+            timestamps: false,
+        }
     );
-  
+
     SendMsg.associate = function () {
-         // belongsTo
-         SendMsg.belongsTo(app.model.Todos, { foreignKey: 'todoId'})
+        // belongsTo
+        app.model.SendMsg.belongsTo(app.model.Todos,
+            {
+                foreignKey: 'todoId',
+                targetKey: 'id',
+            })
     };
-  
+
     return SendMsg;
-  };
-  
+};
