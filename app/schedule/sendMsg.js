5cdf15a6cb01758dfd9170436486189a6ea34511
lining <365388127@qq.com>
2021-04-15 18:25:24 +0800

    4.15

--- app/schedule/sendMsg.js
+++ app/schedule/sendMsg.js
@@ -1,11 +1,11 @@
 module.exports = app => {
-    // return {
-    //     schedule: {
-    //         interval: '1m', // 1 分钟间隔
-    //         type: 'all', // 指定所有的 worker 都需要执行
-    //     },
-    //     async task(ctx) {
-    //         ctx.service.home.sendMsg();
-    //     },
-    // }
+    return {
+        schedule: {
+            interval: '1m', // 1 分钟间隔
+            type: 'all', // 指定所有的 worker 都需要执行
+        },
+        async task(ctx) {
+            ctx.service.home.sendMsg();
+        },
+    }
 };
